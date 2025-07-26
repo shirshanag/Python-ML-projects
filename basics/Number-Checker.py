0d6b2df1960ec52cf407d82af071dc087cacba1a
Shirsha Nag <154455132+shirshanag@users.noreply.github.com>
2025-07-27 00:13:29 +0530

    Create Number-Checker.py

---
+++ basics/Number-Checker.py
@@ -0,0 +1,8 @@
+#To check wether a number is positive negetive or not
+num=int(input("Enter a number:"))
+if(num>0):
+  print("Number is positive")
+elif(num<0):
+  print("Number is negetive")
+else:
+  print("Number is Zero")
